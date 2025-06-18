6165cd0f8bcc95d5b32c37e2b66b0159dfac9c98
MatiasOjman <167230970+MatiasOjman@users.noreply.github.com>
2025-06-18 09:04:54 -0300

    terminado clase 1

---
+++ Assets/introArrays.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introArrays : MonoBehaviour
+{
+    public int[] edades;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(edades[0]);
+        Debug.Log("El array tiene "+ edades.Length + " elementos");
+        Debug.Log(edades[edades.Length - 1]);
+        for (int i = 0; i < edades.Length; i++)
+        {
+            Debug.Log(edades[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/repasoFor.cs
+++ Assets/repasoFor.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class repasoFor : MonoBehaviour
-{
+{ public int num1;
+    public int num2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 2; i <= 40; i += 2)
+        {
+            Debug.Log(i);
+        }
+        //multiplos
+        for (int i=num1;i<num2;i+=num1)
+        {
+            Debug.Log(i);
+        }
+        //saltear
+        for (int i = 100; i>=10;i--)
+        {
+           if (i == 33)
+            {
+                continue;
+            }
+            Debug.Log(i);
+        }
+
     }
 
     // Update is called once per frame
